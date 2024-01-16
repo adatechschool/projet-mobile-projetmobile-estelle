fe4f8bfaee5356f8abc72e752ee115bc297e4f79
Estelle717 <estelle.teglia@gmail.com>
2024-01-16 17:43:37 +0100

    premiere etape projet

--- TravelSpots/App.js
+++ TravelSpots/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 40 }}>
+      <Image 
+        source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
+        style={{ height:300, width: 300 }}
+      ></Image>
+      <Text>Milford Sound</Text>
+      <Text>Fiordland National Park, New Zealand</Text>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
