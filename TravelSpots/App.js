ae0024439f56cbd7474b7b8ac013ac1dff489d38
Estelle717 <estelle.teglia@gmail.com>
2024-01-25 17:35:03 +0100

    changing travel spots and cleaning app.js

--- TravelSpots/App.js
+++ TravelSpots/App.js
@@ -4,68 +4,26 @@ import travelSpotsList from './travelspots.json'
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      
-        <StatusBar backgroundColor="black" barStyle="light-content"></StatusBar>
-        {/* <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.newzealand.com/assets/Tourism-NZ/Other/img-1536221234-1203-7134-p-A39EBFDA-AA01-F68F-7D7FA6CDC3C2406D-2544003__aWxvdmVrZWxseQo_CropResizeWzEyMDAsbnVsbCw3NSwianBnIl0.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View> */}
-        <View>
-          <SectionList
-            sections={travelSpotsList}
-            renderItem={({item}) => {
-              return (
-                <View style={styles.card}>
-                  <Image style={styles.picture} src={item.picture}></Image>
-                  <Text style={styles.cardTitle}>{item.place}</Text>
-                  <Text style={styles.cardLocation}>{item.location}</Text>
-                </View>
-              )
-            }}
-            renderSectionHeader={({ section }) => (
-              <Text style={styles.renderSectionHeader}>{section.continent}</Text>
-            )}
-            ItemSeparatorComponent={()=><View style={{ height: 16 }}></View>}
-            SectionSeparatorComponent={()=><View style={{ height: 16 }}></View>}
-          ></SectionList>
-        </View>
-      
+      <StatusBar backgroundColor="black" barStyle="light-content"></StatusBar>
+      <View>
+        <SectionList
+          sections={travelSpotsList}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.card}>
+                <Image style={styles.picture} src={item.picture}></Image>
+                <Text style={styles.cardTitle}>{item.place}</Text>
+                <Text style={styles.cardLocation}>{item.location}</Text>
+              </View>
+            )
+          }}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.renderSectionHeader}>{section.continent}</Text>
+          )}
+          ItemSeparatorComponent={()=><View style={{ height: 16 }}></View>}
+          SectionSeparatorComponent={()=><View style={{ height: 16 }}></View>}
+        ></SectionList>
+      </View>
     </SafeAreaView>
   )
 }
