3000d96ecce3cc0ea02854ba0ebc5b543f37fc30
Estelle717 <estelle.teglia@gmail.com>
2024-01-25 10:54:33 +0100

    reformatting with SectionList

--- TravelSpots/App.js
+++ TravelSpots/App.js
@@ -1,13 +1,22 @@
-import { View, Text, Image, StyleSheet, StatusBar, SafeAreaView, ScrollView } from "react-native";
+import { View, Text, Image, StyleSheet, StatusBar, SafeAreaView, ScrollView, SectionList } from "react-native";
+import travelSpotsList from './travelspots.json'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      
         <StatusBar backgroundColor="black" barStyle="light-content"></StatusBar>
-        <View style={styles.card}>
+        {/* <View style={styles.card}>
           <Image 
             source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
+            style={styles.picture}
+          ></Image>
+          <Text style={styles.cardTitle}>Milford Sound</Text>
+          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
+        </View>
+        <View style={styles.card}>
+          <Image 
+            source={{ uri:"https://www.newzealand.com/assets/Tourism-NZ/Other/img-1536221234-1203-7134-p-A39EBFDA-AA01-F68F-7D7FA6CDC3C2406D-2544003__aWxvdmVrZWxseQo_CropResizeWzEyMDAsbnVsbCw3NSwianBnIl0.jpg" }}
             style={styles.picture}
           ></Image>
           <Text style={styles.cardTitle}>Milford Sound</Text>
@@ -36,16 +45,27 @@ export default function App() {
           ></Image>
           <Text style={styles.cardTitle}>Milford Sound</Text>
           <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
+        </View> */}
+        <View>
+          <SectionList
+            sections={travelSpotsList}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.card}>
+                  <Image style={styles.picture} src={item.picture}></Image>
+                  <Text style={styles.cardTitle}>{item.place}</Text>
+                  <Text style={styles.cardLocation}>{item.location}</Text>
+                </View>
+              )
+            }}
+            renderSectionHeader={({ section }) => (
+              <Text style={styles.renderSectionHeader}>{section.continent}</Text>
+            )}
+            ItemSeparatorComponent={()=><View style={{ height: 16 }}></View>}
+            SectionSeparatorComponent={()=><View style={{ height: 16 }}></View>}
+          ></SectionList>
         </View>
-        <View style={styles.card}>
-          <Image 
-            source={{ uri:"https://www.milford-sound.co.nz/wp-content/uploads/2017/05/Milford-Sound-reflection.jpg" }}
-            style={styles.picture}
-          ></Image>
-          <Text style={styles.cardTitle}>Milford Sound</Text>
-          <Text style={styles.cardLocation}>Fiordland National Park, New Zealand</Text>
-        </View>
-      </ScrollView>
+      
     </SafeAreaView>
   )
 }
@@ -76,5 +96,10 @@ const styles = StyleSheet.create({
   },
   cardLocation: {
     fontSize: 15,
+  },
+  renderSectionHeader: {
+    backgroundColor: "white",
+    fontSize: 24,
+    fontWeight: "bold"
   }
 })
